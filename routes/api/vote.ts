2bf12ba6c9ebd57860f8136520cac4741c769cba
Zachary Kohnen <me@dusterthefirst.com>
2024-11-02 13:28:17 +0100

    Force maps into json, before deciding its not worth it

--- routes/api/vote.ts
+++ routes/api/vote.ts
@@ -2,8 +2,50 @@ import { FreshContext, Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers<null> = {
   async POST(req, ctx) {
-    req.formData();
+    const form_data = await req.formData();
+    const entries = new Map(form_data.entries());
+    console.log(entries);
 
-    return Response.error()
+    return new Response(
+      JSON.stringify(entries, json_extended_replacer),
+      { headers: { "content-type": "application/json" } },
+    );
+  },
+};
+
+function json_extended_replacer(_key: string, value: unknown) {
+  if (value instanceof Map) {
+    return {
+      _json_extended_data_type: "Map",
+      _json_extended_entries: Object.fromEntries(value.entries()),
+    };
+  } else {
+    return value;
   }
+}
+
+function hasOwnTyped<O extends object, const T extends string>(
+  object: O,
+  key: T,
+): object is O & { [x in T]: unknown } {
+  return Object.hasOwn(object, key);
+}
+
+function json_extended_reviver(key: string, value: unknown) {
+  if (typeof value === "object" && value !== null) {
+    if (
+      hasOwnTyped(value, "_json_extended_data_type") &&
+      value._json_extended_data_type === "Map" &&
+      hasOwnTyped(value, "_json_extended_entries") &&
+      typeof value._json_extended_entries === "object"
+    ) {
+      if (value._json_extended_entries === null) {
+        return null;
+      }
+
+      return new Map(Object.entries(value._json_extended_entries));
+    }
+  }
+
+  return value;
 }
